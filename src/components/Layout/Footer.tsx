0290a0cc6c6f5b4fe8c10eefe555df7008eda296
HyeongSeok <gudtjr3437@naver.com>
2022-07-14 17:02:11 +0900

    Feat : Footer component

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,12 @@
+import Nav from 'components/Nav'
+import styles from './layout.module.scss'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footerContainer}>
+      <Nav />
+    </footer>
+  )
+}
+
+export default Footer

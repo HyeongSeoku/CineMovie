605524b27d89fe5034b85c92e385ca3a8dac0547
HyeongSeok <gudtjr3437@naver.com>
2022-07-17 20:45:43 +0900

    Feat : Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,23 @@
+import Layout from 'components/Layout'
+import { Routes, Route } from 'react-router-dom'
+
+import Account from './Account'
+import Bookmark from './Bookmark'
+import Main from './Main'
+import Play from './Play'
+import Search from './Search'
+
 const App = () => {
   return (
-    <div className='App'>
-      <div>메인</div>
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route path='/' element={<Main />} />
+        <Route path='play' element={<Play />} />
+        <Route path='search' element={<Search />} />
+        <Route path='bookmark' element={<Bookmark />} />
+        <Route path='account' element={<Account />} />
+      </Route>
+    </Routes>
   )
 }
 
